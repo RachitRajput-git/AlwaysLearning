0517243c216949b25aebc5ea95daaf524db2f2ef
unknown <rahul.lodhi@socwise.com>
2020-11-12 15:19:42 +0530

    Updates

---
+++ precache-manifest.04ae9c2dcbf3d1edfc4faddff2ce9d49.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9b5bd6aae9f146ec4c017eac9dcc38",
+    "url": "/AlwaysLearning/index.html"
+  },
+  {
+    "revision": "0f22af5fa3bca0dc8bac",
+    "url": "/AlwaysLearning/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "a8fd2016398f845a687c",
+    "url": "/AlwaysLearning/static/css/main.ba8b44f8.chunk.css"
+  },
+  {
+    "revision": "0f22af5fa3bca0dc8bac",
+    "url": "/AlwaysLearning/static/js/2.34ca0737.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/AlwaysLearning/static/js/2.34ca0737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fd2016398f845a687c",
+    "url": "/AlwaysLearning/static/js/main.b05b9206.chunk.js"
+  },
+  {
+    "revision": "4999a8c3f6dfad2270c2",
+    "url": "/AlwaysLearning/static/js/runtime-main.521b3131.js"
+  },
+  {
+    "revision": "66acf44c6ebd4619b2f5cb339959b4c2",
+    "url": "/AlwaysLearning/static/media/Jaguar.66acf44c.jpg"
+  },
+  {
+    "revision": "fd1781590df4476a262dc4ffe2908dce",
+    "url": "/AlwaysLearning/static/media/Logo.fd178159.png"
+  },
+  {
+    "revision": "11293e0fed623c0b5f3b17cd01b66a7e",
+    "url": "/AlwaysLearning/static/media/banner.11293e0f.png"
+  },
+  {
+    "revision": "3c482d2df4196966a0f41e742dc6e677",
+    "url": "/AlwaysLearning/static/media/footerBanner.3c482d2d.jpg"
+  },
+  {
+    "revision": "df4fc9d0e977d99508fa3d1587287e35",
+    "url": "/AlwaysLearning/static/media/slider.df4fc9d0.jpg"
+  },
+  {
+    "revision": "8fca7058a46b80ca5edcfbd11bf3701d",
+    "url": "/AlwaysLearning/static/media/slider_2.8fca7058.jpg"
+  }
+]);
